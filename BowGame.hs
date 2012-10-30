9985b091ff48e4fb4fb0fb77d9b6964f8bb5946d
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-10-30 11:23:20 +0900

    Vec,PosをPoint2に変更

--- BowGame.hs
+++ BowGame.hs
@@ -2,9 +2,9 @@ module BowGame where
 
 import Data.List ((\\))
 import Data.Maybe
+import Graphics.UI.WX (Bitmap,Point2(..),point)
 
 data Game a = Game a | End
-data Vec a = Vec {x :: a , y :: a} deriving (Eq,Ord)
 data Command =
       Jump
 	| Shoot
@@ -15,8 +15,17 @@ data Command =
 	| Release
 	 deriving Eq
 
-type Pos = Vec
+type Pos = Point2
+type Vec = Point2
+
 type Enemy = (Pos Int,Vec Int)
+
+data BitMapInfo = BitMapInfo {
+	manBM   :: Bitmap(),
+	bowBM   :: Bitmap(),
+	enemyBM :: Bitmap(),
+	numBM :: Bitmap()
+	}
 
 data GameState = GameState {
 	score :: Int,
@@ -31,10 +40,10 @@ instance Show GameState where
 	show s = "POINT :" ++ show (score s)++ "\n LIFE :" ++ show (life s)
 
 add :: (Num a) => Pos a -> Vec a -> Pos a
-add p v = Vec (x p + x v) (y p + y v)
+add p v = point (pointX p + pointX v) (pointY p + pointY v)
 
 distance :: Pos Int -> Pos Int -> Float --(Integral a , Integral b) => Pos a -> Pos a -> b
-distance p1 p2 = sqrt.fromIntegral $ (x p1 - x p2)^2 + (y p1 - y p2)^2
+distance p1 p2 = sqrt.fromIntegral $ (pointX p1 - pointX p2)^2 + (pointY p1 - pointY p2)^2
 
 aliveEnemies :: GameState -> (Int,[Enemy])
 aliveEnemies s = if bow s == Nothing || destroyed == []
@@ -64,16 +73,16 @@ updateGame (Game s) cm = if life s < 0
 		(bow',power') = nextBowState s cm
 
 nextPos :: Pos Int -> Command -> Pos Int
-nextPos p (Walk x) = add p (Vec x $ y p)
+nextPos p (Walk x) = add p (point x $ pointY p)
 
 --矢の位置と飛力を渡す
 nextBowState :: GameState -> Command -> (Maybe (Pos Int) , Vec Int)
 nextBowState s Release = (Just (pos s) , power s)
-nextBowState s (Drawing x y) = (bow s , add (power s) $ Vec x y )
+nextBowState s (Drawing x y) = (bow s , add (power s) $ point x y )
 nextBowState s _ = if 0 < fst (aliveEnemies s)  --矢が敵にヒット
-	then (Nothing,Vec 0 0)
+	then (Nothing,point 0 0)
 	else (Just $ add (power s) $ fromJust (bow s) , attenuate $ power s)
 
 --矢の推進力の減衰関数
 attenuate :: Vec Int -> Vec Int
-attenuate (Vec x y) = Vec x $ min 0 (abs $ y - 10)
+attenuate p = point (pointX p) $ min 0 (abs $ pointY p - 10)
