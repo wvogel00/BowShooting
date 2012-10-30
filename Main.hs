ae31c811c7b3934605b6c829cc37ff91cb5acacb
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-10-30 11:24:36 +0900

    Bitmapデータ生成を追加

--- Main.hs
+++ Main.hs
@@ -1,14 +1,25 @@
 import Graphics.UI.WX
 import Graphics.UI.WXCore (bitmapGetSize)
-
+import BowGame
 
 main = start bowGUI
 
 bowGUI :: IO()
 bowGUI = do
-	manBM <- bitmapCreateFromFile "img/stand.png"
-	size <- bitmapGetSize manBM
-	f <- frame [text := "弓撃ち", on paint := onPaint manBM, clientSize := sz 600 320]
-	repaint f where
-		onPaint :: Bitmap() -> DC() -> Rect -> IO()
-		onPaint bm dc area = drawBitmap dc bm pointZero False []
+	bmInfo <- initBitMap
+	size <- bitmapGetSize (manBM bmInfo)
+	f <- frame [text := "弓撃ち", on paint := onPaint (point 10 10,manBM bmInfo), clientSize := sz 600 320]
+	repaint f
+
+onPaint :: (Point2 Int,Bitmap()) -> DC() -> Rect -> IO()
+onPaint (pt,bm) dc area = drawBitmap dc bm pt False []
+
+initBitMap :: IO BitMapInfo
+initBitMap = do
+	man <- bitmapCreateFromFile "img/stand.png"
+	bow <- bitmapCreateFromFile "img/stand.png"
+	enemy <- bitmapCreateFromFile "img/stand.png"
+	nums <- bitmapCreateFromFile "img/stand.png"
+	return $ BitMapInfo{
+		manBM = man, bowBM = bow, enemyBM = enemy,numBM = nums
+		}
