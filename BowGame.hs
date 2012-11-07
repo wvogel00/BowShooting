97ebea375a75be32401404e296254b0a62eb0c95
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-11-07 21:37:23 +0900

    GameStateを変更

--- BowGame.hs
+++ BowGame.hs
@@ -1,8 +1,9 @@
 module BowGame where
 
-import Data.List ((\\))
+import Data.List ((\\),delete)
 import Data.Maybe
 import Graphics.UI.WX (Bitmap,Point2(..),point)
+import Control.Applicative
 
 data Game a = Game a | End
 data Command =
@@ -15,29 +16,32 @@ data Command =
 	| Release
 	 deriving Eq
 
+type Bow = (Pos Int,Vec Int)
 type Pos = Point2
 type Vec = Point2
 
 type Enemy = (Pos Int,Vec Int)
 
-data BitMapInfo = BitMapInfo {
-	manBM   :: Bitmap(),
-	bowBM   :: Bitmap(),
-	enemyBM :: Bitmap(),
-	numBM :: Bitmap()
-	}
-
 data GameState = GameState {
 	score :: Int,
-	power :: Vec Int,
+	power :: Maybe (Vec Int),
 	life :: Int,
 	pos :: Pos Int,
-	bow :: Maybe (Pos Int),
+	bows :: [Bow],
 	enemies :: [Enemy]
 } deriving (Eq,Ord)
 
 instance Show GameState where
 	show s = "POINT :" ++ show (score s)++ "\n LIFE :" ++ show (life s)
+
+initGameState = GameState{
+	score = 0,
+	power = Nothing,
+	life = 3,
+	pos = point 50 240,
+	bows = [],
+	enemies = []
+	}
 
 add :: (Num a) => Pos a -> Vec a -> Pos a
 add p v = point (pointX p + pointX v) (pointY p + pointY v)
@@ -46,12 +50,13 @@ distance :: Pos Int -> Pos Int -> Float --(Integral a , Integral b) => Pos a -> 
 distance p1 p2 = sqrt.fromIntegral $ (pointX p1 - pointX p2)^2 + (pointY p1 - pointY p2)^2
 
 aliveEnemies :: GameState -> (Int,[Enemy])
-aliveEnemies s = if bow s == Nothing || destroyed == []
+aliveEnemies s = if bows s == [] || destroyed == []
 	then (0,enemies s) else (length destroyed,enemies s\\destroyed)
 	where
-		reachPt = add (fromJust $ bow s) $ power s
-		inHitArea (p,_) = distance reachPt p < 10
-		destroyed = filter inHitArea $ enemies s
+		reachPoses = map (\(p,v) -> (add p $point 5 5,v)) $ bows s
+		inHitArea (p1,_) p2 = distance p1 p2 < 10
+		destroyed = filter alives $ enemies s
+		alives enem = any (inHitArea enem) $ map fst reachPoses
 
 hitOnPlayer :: GameState -> Bool
 hitOnPlayer s = (/=[]).filter inHitArea $ enemies s where
@@ -63,25 +68,23 @@ updateGame (Game s) cm = if life s < 0
 	then End
 	else Game GameState {
 		score = score s + point*50,
-		power = power',
+		power = power s,
 		life = if hitOnPlayer s then life s -1 else life s,
 		pos = nextPos (pos s)  cm,
-		bow = bow',
+		bows = mkNextBows s cm $ bows s,
 		enemies = alives
 	} where
 		(point,alives) = aliveEnemies s
-		(bow',power') = nextBowState s cm
 
 nextPos :: Pos Int -> Command -> Pos Int
 nextPos p (Walk x) = add p (point x $ pointY p)
 
---矢の位置と飛力を渡す
-nextBowState :: GameState -> Command -> (Maybe (Pos Int) , Vec Int)
-nextBowState s Release = (Just (pos s) , power s)
-nextBowState s (Drawing x y) = (bow s , add (power s) $ point x y )
-nextBowState s _ = if 0 < fst (aliveEnemies s)  --矢が敵にヒット
-	then (Nothing,point 0 0)
-	else (Just $ add (power s) $ fromJust (bow s) , attenuate $ power s)
+--矢の位置とその矢の飛力を返す
+mkNextBows :: GameState -> Command -> [Bow] -> [Bow]
+mkNextBows s Release bs = (pos s,fromJust $ power s):bs
+mkNextBows s _ bs = if 0 < fst (aliveEnemies s)  --矢が敵にヒット
+	then foldl (flip delete) bs $ snd $ aliveEnemies s
+	else bs
 
 --矢の推進力の減衰関数
 attenuate :: Vec Int -> Vec Int
