908be6ab8dad5ef0df80e528d2bc804db0cf0e50
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-11-08 00:03:14 +0900

    イベントによる描画更新の実装を開始

--- Main.hs
+++ Main.hs
@@ -8,15 +8,16 @@ main = start bowGUI
 bowGUI :: IO()
 bowGUI = do
 	state <- newIORef (End Continue)
-	f <- frame [text := "shooting",
-				on paint := onPaint state,
-				clientSize := sz 600 320,
-				on click := clickEvent state,
-				on rightKey := moveObject state 5,
-				on leftKey := moveObject state (-5)]
 
-	t <- timer f [interval := 25 ,on command := updateGameState state]
-	repaint f
+	f <- frame [text := "shooting",clientSize:=sz width height]
+	p <- panel f [on paint := onPaint state]
+	t <- timer f [interval := 25 ,on command := updateGameState state p]
+
+	set p [ on click := clickEvent state f,
+			on enterKey := clickEvent' state f,
+			on rightKey := moveObject state 5,
+			on leftKey  := moveObject state (-5)]
+	--set f [layout := minsize (sz width height) $ widget p]
 
 --IORefで渡しているので、draw関数を修正する必要、またはonPaint関数を削除する可能性あり
 onPaint :: IORef (Game GameState) -> DC() -> Rect -> IO()
@@ -45,36 +46,42 @@ movePlayer sIO k = do
 	modifyIORef sIO (move k) where
 		move k (Game st) = Game $ st {pos = add (pos st) (pt k 0)}
 
+clickEvent' :: IORef (Game GameState) -> Frame () -> IO()
+clickEvent' sIO f = do
+	s <- readIORef sIO
+	case s of
+		Game _ -> return ()
+		_ -> clickEvent sIO f (pt 0 0)
+
 --左マウスクリックイベント（矢を番える、start,end画面の更新）
-clickEvent :: IORef (Game GameState) -> Point -> IO()
-clickEvent sIO pt = do
+clickEvent :: IORef (Game GameState) -> Frame ()-> Point -> IO()
+clickEvent sIO f pt = do
 	s <- readIORef sIO
 	case s of
 		Start _ -> writeIORef sIO (Game initGameState)
-		End _ -> writeIORef sIO (Start $ point 160 100)
+		End Continue -> writeIORef sIO (Start $ point 160 100)
+		End Quit -> close f
 		Game _ -> modifyIORef sIO initPower where
 			initPower (Game st) = Game $ st {power = Just pt}
 
 --オープニングを描画
 drawOpening :: Point2 Int -> DC() -> Rect -> IO()
 drawOpening pos dc area = do
-	drawRect dc (rect (pt 0 0) (sz 600 320)) [brushColor:=rgb 160 45 12]
+	drawRect dc (rect (pt 0 0) (sz width height)) [brushColor:=rgb 160 45 12]
 	drawText dc "弓撃ちゲーム" (pt 160 100) [brushColor:=green,font:=fontFixed{_fontSize=25}]
 
 --ゲームオーバー画面描画
 drawEnd :: Choise -> DC() -> Rect -> IO()
 drawEnd lr dc area = do
 	let pointer = [pt 60 248,pt 60 272,pt 90 260,pt 60 248]
-	drawRect dc (rect (pt 0 0) (sz 600 320)) [brushColor:=rgb 20 20 40]
+	drawRect dc (rect (pt 0 0) (sz width height)) [brushColor:=rgb 20 20 40]
 	drawText dc "ゲームオーバー" (pt 160 100) [color:=red,brushColor:=red,font:=fontFixed{_fontSize=25}]
 	drawText dc "続ける" (pt 100 240) [color:=white,font:=fontFixed{_fontSize=20}]
 	drawText dc "やめる" (pt 400 240) [color:=white,font:=fontFixed{_fontSize=20}]
 
 	case lr of
 		Continue -> polygon dc pointer [brushColor:=yellow]
-		Quit -> polygon dc (map (add (point 200 0)) pointer) [brushColor:=blue]
+		Quit -> polygon dc (map (add (point 290 0)) pointer) [brushColor:=yellow]
 
-
-
-updateGameState :: IORef (Game GameState) -> IO()
-updateGameState sIO = return ()
+updateGameState :: IORef (Game GameState) -> Panel() -> IO()
+updateGameState sIO p = repaint p
