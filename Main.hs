ca13b0b1c1635bbf5470e71507b1852673237741
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-11-07 21:36:54 +0900

    描画関数を編集、Bitmap読み込みを削除

--- Main.hs
+++ Main.hs
@@ -1,25 +1,16 @@
 import Graphics.UI.WX
 import Graphics.UI.WXCore (bitmapGetSize)
 import BowGame
+import BowGameDrawer
 
 main = start bowGUI
 
 bowGUI :: IO()
 bowGUI = do
-	bmInfo <- initBitMap
-	size <- bitmapGetSize (manBM bmInfo)
-	f <- frame [text := "弓撃ち", on paint := onPaint (point 10 10,manBM bmInfo), clientSize := sz 600 320]
+	f <- frame [text := "shooting", on paint := onPaint (point 10 10), clientSize := sz 600 320]
 	repaint f
 
-onPaint :: (Point2 Int,Bitmap()) -> DC() -> Rect -> IO()
-onPaint (pt,bm) dc area = drawBitmap dc bm pt False []
-
-initBitMap :: IO BitMapInfo
-initBitMap = do
-	man <- bitmapCreateFromFile "img/stand.png"
-	bow <- bitmapCreateFromFile "img/stand.png"
-	enemy <- bitmapCreateFromFile "img/stand.png"
-	nums <- bitmapCreateFromFile "img/stand.png"
-	return $ BitMapInfo{
-		manBM = man, bowBM = bow, enemyBM = enemy,numBM = nums
-		}
+onPaint :: Point2 Int -> DC() -> Rect -> IO()
+onPaint pt dc area = do
+	drawArchery (point 140 220) 0 dc area
+	draw initGameState dc area
