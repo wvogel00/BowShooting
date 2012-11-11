b31bbdc5b17bb922b68b9a1fa628f2485146c3b2
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-11-11 19:28:10 +0900

    パーサー

--- DataParser.hs
+++ DataParser.hs
@@ -3,7 +3,7 @@ module DataParser where
 import BowGame
 import Text.Parsec
 import Text.Parsec.String
-import qualified Graphics.UI.WX as WX hiding (color)
+import Graphics.UI.WX hiding (color)
 import Control.Applicative hiding ((<|>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Char (toUpper)
@@ -12,25 +12,29 @@ getData :: String -> IO (Maybe [Enemy])
 getData filedata = do
 	case (parse mkData "" filedata) of
 		Left err -> print err >> return Nothing
-		Right xs -> return (Just xs)
+		Right xs -> return $ Just $ map convertPos xs
 
-mkData :: Parser [Enemy] = do
-	try ( do
-		pos <- WX.pt<$>num<*>(spaces *> num)
-		vec <- WX.pt<$>(spaces *> num)<*>(spaces *> num)
-		r <- spaces *> num <* spaces
-		cl <- color.map toUpper<$>many1 letter
-		spaces
-		(:) (pos,vec,r,cl) <$> mkData
-		)
-	<|> return []
+convertPos (p,v,r,c) = (pt (convX p) (convY p), v, r, c)
+convX p = width - pointX p
+convY p = if pointY p < 50 then pointY p+50 else pointY p
+
+mkData :: Parser [Enemy]
+mkData = try ( do
+	pos <- pt<$>num<*>(spaces *> num)
+	vec <- pt<$>(spaces *> num)<*>(spaces *> num)
+	r <- spaces *> num <* spaces
+	cl <- color.map toUpper<$>many1 letter
+	spaces
+	(:) (pos,vec,r,cl) <$> mkData
+	)
+	<|> (string "end" >> return [])
 
 num :: Parser Int
 num = read<$>many1 digit
 
-color :: String -> WX.Color
-color "RED" = WX.red
-color "BLUE" =WX.blue
-color "BLACK" = WX.black
-color "CYAN"= WX.cyan
-color "GREEN" = WX.green
+color :: String -> Color
+color "RED" = red
+color "BLUE" =blue
+color "BLACK" = black
+color "CYAN"= cyan
+color "GREEN" = green
